1d1c49159bc6755c94ad93fc3fae5a5b94d452e5
iamnaymur <nrtusher1@gmail.com>
2023-09-01 17:14:15 +0600

    tryout server'=

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "font-db";
+
+// create connection
+
+$connection = new mysqli($servername, $username, $password, $database);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+?>
